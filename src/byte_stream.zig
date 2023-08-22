1bcb003adcbb3c4bbe7d0702e320083cc2bf76d2
unnask <8060462+unnask@users.noreply.github.com>
2023-08-22 14:09:33 -0600

    add notes

--- src/byte_stream.zig
+++ src/byte_stream.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const testing = std.testing;
 
+// this should follow the general stream ideas as seen in std.io.
+
+//read()
+//reader()
+//write()
+//writeAssumeCapacity()
+//writer()
+//writerAssumeCapacity()
+// Main difference being that we're providing writers that can either
+// grow the backing array, or assume capacity as necessary.
+//
+
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
